43d0a6a13a828c0029410162e5a778f71a65e599
Michael Hargiss <mdhargiss@gmail.com>
2015-09-16 16:04:35 -0600

    add specs

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,24 +1,26 @@
 require_relative '../rails_helper'
 
-describe 'books index page' do
-  before(:each) do
-    visit 'books'
-  end
+describe 'creating new books' do
 
-  it 'contains the text "Listing Books"' do
-    expect(page).to have_content 'Listing Books'
-  end
+  context 'on the books index page' do
+    before(:each) do
+      visit 'books'
+    end
 
-  it 'has a link for creating a new book' do
-    expect(page).to have_link 'New Book'
-  end
+    it 'contains the text "Listing Books"' do
+      expect(page).to have_content 'Listing Books'
+    end
 
-  context 'when on the index page', :aggregate_failures do
+    it 'has a link for creating a new book' do
+      expect(page).to have_link 'New Book'
+    end
+
     it 'creates a new book' do
       click_link 'New Book'
 
-      expect(page).to have_content 'New Book fail'
-      expect(current_url).to have_content '/books/new/fail'
+      expect(page).to have_content 'New Book'
+      expect(current_url).to have_content '/books/new'
     end
   end
+
 end
